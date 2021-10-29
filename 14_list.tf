870994b74b43756e21240c528970ce3e375861e7
sonjw <geee94@naver.com>
2021-10-29 15:05:06 +0900

    list_step14

---
+++ 14_list.tf
@@ -0,0 +1,12 @@
+resource "aws_lb_listener" "sonjw_lblist" {
+  load_balancer_arn = aws_lb.sonjw_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+#  ssl_policy        = "ELBSecurityPolicy-2016-08"
+#  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.sonjw_lbtg.arn
+  }
+}
